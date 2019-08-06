7a884ea8c9f55a543a949316a4d49a908ec2e191
Pavel Smokorovskiy <pavvel@gmail.com>
2019-08-06 17:26:13 +0300

    added console inputs

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
+
+    private Logger logger = Logger.getLogger(Main.class.getName());
 
 	public static void main(String[] args) {
 
+        new Main().run();
+    }
+
+    private void run() {
+        String firstNumber = getConsoleInput();
+        String secondNumber = getConsoleInput();
+        logger.log(Level.INFO, firstNumber);
+        logger.log(Level.INFO, secondNumber);
+    }
+
+    private String getConsoleInput() {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = null;
+        try {
+            name = reader.readLine();
+        } catch (IOException e) {
+            logger.log(Level.WARNING, "Wrong input: ", e);
+        }
+        return name;
     }
 }
